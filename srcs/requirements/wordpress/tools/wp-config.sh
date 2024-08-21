e9d9f46354a1de304b9848991e69c805efa669d9
SalahHW <salah.bouheniche@gmail.com>
2024-08-21 21:28:23 +0200

    Update wp-config.sh to create WordPress configuration file with database details

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -1,7 +1,21 @@
+sleep 10
+
+wp core download \
+--locale=fr_FR \
+--force
+
 wp config create \
 --allow-root \
---dbname=$SQL_DATABASE \
---dbuser=$SQL_USER \
---dbpass=$SQL_PASSWORD \
+--dbname=$DB_NAME \
+--dbuser=$DB_USER \
+--dbpass=$DB_USER_PASSWORD \
 --dbhost=mariadb:3306 \
---path='/var/www/wordpress'
+--force
+
+wp core install \
+--url=$WP_URL \
+--title=$WP_TITLE \
+--admin_user=$WP_ADMIN \
+--admin_password=$WP_ADMIN_PASSWORD \
+--admin_email=$WP_ADMIN_EMAIL \
+--skip-email 
